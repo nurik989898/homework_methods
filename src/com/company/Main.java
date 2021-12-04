ae8532aa9f6e83c0398e97a3440342aba97e3c4e
Nurik Turdukulov <nurikturdukulov@Nurikui-MacBookPro.local>
2021-12-05 01:03:34 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(car(23,11));
+    }
+        public static String car(int age, int temperature){
+        return age + " " + temperature;
+    }
+}
